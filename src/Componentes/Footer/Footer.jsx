d360db21e3948cddda281bd9b79e31b4b3123a30
Alexander <alexandergarciaclei5b@gmail.com>
2022-11-18 10:22:02 -0500

    se avanza en el footer y se ensayan animaciones

--- src/Componentes/Footer/Footer.jsx
+++ src/Componentes/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Footer.css'
+import { GithubLogo, Alien, FacebookLogo } from "phosphor-react";
 
 const Footer = () => {
   return (
     <div>
-      <a></a>
+      <a className='' href=''> <GithubLogo size={32} color="#030303" weight="bold" /> </a>
+      <a className='' href=''> <Alien size={32} color="#030303" weight="bold" /> </a>
+      <a className='' href=''> <FacebookLogo size={32} color="#030303" weight="bold" /></a>
+      <a className='' href=''> <GithubLogo size={32} color="#030303" weight="bold" /> </a>
     </div>
   )
 }
