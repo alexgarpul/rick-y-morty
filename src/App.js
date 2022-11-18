6e44a13862209b8edaee929d5a6dfd937ae731fa
LozanoAndersonTheStain <adslozanov@gmail.com>
2022-11-18 07:28:48 -0500

    se cambian estilos del app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Personajes from './Componentes/Personajes/Personajes'
 import './app.css'
+import Footer from './Componentes/Footer/Footer.jsx'
 
 const App = () => {
   return (
-    <div className='fondo' >
-      <h1 className='title'> RUBIOXXX </h1>
-      <Personajes/>
+    <div className="fondo">
+      <h1 className="title"> RUBIOXXX </h1>
+      <Personajes />
+      <Footer />
     </div>
-  
   )
 }
 
---
+++ src/Componentes/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+      <a></a>
+    </div>
+  )
+}
+
+export default Footer
